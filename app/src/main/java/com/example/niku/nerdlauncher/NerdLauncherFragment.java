99ce55598be7ac6a8b2bd023a8d4a1099d80b4c7
niku <niku.andrew@gmail.com>
2017-09-22 10:00:39 +0300

    Init

---
+++ app/src/main/java/com/example/niku/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,15 @@
+package com.example.niku.nerdlauncher;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by niku on 22.09.2017.
+ */
+
+public class NerdLauncherFragment extends Fragment {
+
+    public static NerdLauncherFragment newInstance() {
+        return new NerdLauncherFragment();
+    }
+
+}
---
+++ app/src/main/java/com/example/niku/nerdlauncher/SingleFragmentActivity.java
@@ -0,0 +1,41 @@
+package com.example.niku.nerdlauncher;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by niku on 06.12.2016.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+    }
+
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+}
